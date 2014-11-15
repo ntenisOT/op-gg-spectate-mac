e27bd5f50e897e2f38f636b6482cf675f6785c3c
Georgios Larkou <ntenisOT@gmail.com>
2014-11-15 23:00:07 +0200

    Updated versions

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 function readBlob() {
 
-    var str = 'cd "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/0.0.0.143/deploy/LeagueOfLegends.app/Contents/MacOS" && riot_launched=true "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/0.0.0.143/deploy/LeagueOfLegends.app/Contents/MacOS/LeagueofLegends" 8394 LoLLauncher "/Applications/League of Legends.app/Contents/LoL/RADS/projects/lol_air_client/releases/0.0.0.143/deploy/bin/LolClient" ';
+    var str = 'chmod +x "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/0.0.0.143/deploy/LeagueOfLegends.app/Contents/MacOS/LeagueofLegends" && cd "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/0.0.0.143/deploy/LeagueOfLegends.app/Contents/MacOS" && riot_launched=true "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/0.0.0.143/deploy/LeagueOfLegends.app/Contents/MacOS/LeagueofLegends" 8394 LoLLauncher "/Applications/League of Legends.app/Contents/LoL/RADS/projects/lol_air_client/releases/0.0.0.143/deploy/bin/LolClient" ';
 
 
     var files = document.getElementById('files').files;
@@ -26,10 +26,43 @@ function readBlob() {
             var regExp = /"spectator.*"/g;
             var match = regExp.exec(text);
 
-            document.getElementById('byte_content').textContent = str + match[0];
+            if(match === null) {
+                alert('File not supported! Please select another file.')
+            }
+
+            var parent = document.getElementById('code-wrap');
+
+            parent.innerHTML = "";
+
+            var el = document.createElement('div');
+            el.id = 'byte_content';
+            el.innerHTML = '<b>Please Copy and Paste in your Terminal (Application - Utilities - Terminal) the following:</b></br></br>';
+
+            el.innerHTML  = el.innerHTML  +  str + match[0];
+
+            parent.appendChild(el);
+
         }
     };
 
     var blob = file.slice(start, stop + 1);
     reader.readAsBinaryString(blob);
 }
+
+$("#files").fileinput({'showPreview': false, 'uploadLabel': 'Submit', 'maxFileCount': 1});
+
+$(".kv-fileinput-upload").on("click", function() {
+    readBlob();
+});
+
+$("#files").on('fileclear', function(event) {
+    var parent = document.getElementById('code-wrap');
+
+    parent.innerHTML = "";
+
+    var el = document.createElement('div');
+    el.id = 'message_content';
+
+    el.innerHTML = '<b>The command will be available here!</b>';
+    parent.appendChild(el);
+});
